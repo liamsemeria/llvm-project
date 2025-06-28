e18beffd3803e920b71ac9a7ed6f417c5ba5cecb
Liam <liamsemeria@gmail.com>
2025-06-28 15:45:43 -0700

    added print-msg option

--- mlir/test/lib/IR/TestDialectCounts.cpp
+++ mlir/test/lib/IR/TestDialectCounts.cpp
@@ -11,34 +11,41 @@ using namespace mlir;
 namespace {
     struct TestDialectCountsPass : public PassWrapper<TestDialectCountsPass, OperationPass<>> {
     
-    //TestDialectCountsPass() = default;
-    //TestDialectCountsPass(const TestDialectCountsPass& p) = {}
-
-    std::map<StringRef, int> dialect_counts;
-    std::map<StringRef, std::string> dialect_colors;
-    // keep track of hash of each dialect name, rehash in on collision
 
 
     MLIR_DEFINE_EXPLICIT_INTERNAL_INLINE_TYPE_ID(TestDialectCountsPass)
 
+        Option<std::string> printMsg{*this, "print-msg", llvm::cl::desc("if specified will print before colors")};
+
+        TestDialectCountsPass() = default;
+        TestDialectCountsPass(const TestDialectCountsPass& p) : PassWrapper(p) {}
+
         //Option<bool> includeColorKey{*this, "include_color_key", llvm::cl::desc("wether or not to print the color key after printing")}
+        
 
         StringRef getArgument() const final { return "test-dialect-counts"; }
         StringRef getDescription() const final { return "count ops of each dialect."; }
 
         void runOnOperation() override {
+            if (! printMsg.empty()) 
+                llvm::outs() << printMsg << "\n";
+
             // Get the current operation being operated on.
             Operation *op = getOperation();
-            run(op);
+            std::map<StringRef, int> dialect_counts;
+            std::map<StringRef, std::string> dialect_colors;
+
+            run(op, dialect_counts, dialect_colors);
+
+            // print the color key
             llvm::outs() << "\n";
-            // print the color key if requested
             for (auto iter : dialect_counts) {
                 llvm::outs() << dialect_colors[iter.first] << iter.first << ": " << iter.second << " ";
             }
             llvm::outs() << "\n";
         }
 
-        void run(Operation *op) {
+        void run(Operation *op,std::map<StringRef, int>& dialect_counts, std::map<StringRef, std::string>& dialect_colors) {
             // count the dialect for the operation
             StringRef s = op->getDialect()->getNamespace();
             
@@ -58,7 +65,7 @@ namespace {
             for (Region &r : op->getRegions()) {
                 for (Block &b : r.getBlocks()) {
                     for (Operation &o : b.getOperations()) {
-                        run(&o);
+                        run(&o, dialect_counts, dialect_colors);
                     }
                 }
             }
